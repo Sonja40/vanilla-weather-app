8d9fba1452437e68fae6397737b1c5c769089574
Sonja40 <137377644+Sonja40@users.noreply.github.com>
2023-08-31 20:17:04 +0200

    changed Main Icon and setattribute to icon to descrption and the city

--- src/appnew.js
+++ src/appnew.js
@@ -30,19 +30,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   weatherElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "8429fab9a373o690ete5fa53ee0a6e14";
-let city = "Lisbon";
+let city = "Paris";
 let units = "metric";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=8429fab9a373o690ete5fa53ee0a6e14&units=${units}";
+  "https://api.shecodes.io/weather/v1/current?query=Tokyo&key=8429fab9a373o690ete5fa53ee0a6e14&units=metric";
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
